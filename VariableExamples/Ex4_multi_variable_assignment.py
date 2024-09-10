c5551e19d8f6802aa4fa8e3d3baf72bda813c959
jwright57 <wrightjason7575@gmail.com>
2024-09-10 13:10:32 -0400

    Update Ex4_multi_variable_assignment.py

--- VariableExamples/Ex4_multi_variable_assignment.py
+++ VariableExamples/Ex4_multi_variable_assignment.py
@@ -17,3 +17,23 @@ x, y, z = "Apple", 123, 3.14
 print(x)
 print(y)
 print(z)
+
+# Chap2 - Variables
+# Multiple variables
+
+# Initialize multiplee variable using different value
+x, y, z = "Apple", "Banana", "Cherry"
+print(x,y,z)
+
+
+# Initialize multiple variable using same value
+x = y = z = 0
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+xyz = "Apple", 123, 3.14 ##this is a list!
+print(xyz)
+print(y)
+print(z)
