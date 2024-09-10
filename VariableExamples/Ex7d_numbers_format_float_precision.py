a8bdab3f0ea0c84e89c4ebef42454ffa9a5bee4c
jwright57 <wrightjason7575@gmail.com>
2024-09-10 13:49:34 -0400

    Update Ex7d_numbers_format_float_precision.py

--- VariableExamples/Ex7d_numbers_format_float_precision.py
+++ VariableExamples/Ex7d_numbers_format_float_precision.py
@@ -6,14 +6,14 @@
 PI = 3.14
 
 # use % operator
-print('%.1f' % PI)
-print('%.2f' % PI)
-print('%.3f' % PI)
+print('%.1f' % PI) #this is the correct syntax, and it's telling it to print an amount of float points
+print('%.2f' % PI) #so here it'll print 2 decimal places over
+print('%.3f' % PI) #and here it'll print 3. this is to be precise in our values
 print()
 
 # use format() method to format a specified value
-print( "{:.6f} {:.2f} {:.3f}".format(0.123456789, 711, PI) )
-print()
+print( "{:.6f} {:.2f} {:.3f}".format(0.123456789, 711, PI) ) #each precision {:._f} will apply
+print()                                                     # to the corresponding value, in order
 
 # f-string print with various levels of floating point precision
 print(f"{PI:.1f}")
